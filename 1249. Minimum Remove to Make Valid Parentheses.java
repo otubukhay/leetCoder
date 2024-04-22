30e357b01014e20cb99e916c3178920800646da9
otubukhay <33136805+otubukhay@users.noreply.github.com>
2024-04-21 21:36:14 -0500

    Update 1249. Minimum Remove to Make Valid Parentheses.java

--- 1249. Minimum Remove to Make Valid Parentheses.java
+++ 1249. Minimum Remove to Make Valid Parentheses.java
@@ -1,6 +1,6 @@
     public String minRemoveToMakeValid(String s) 
     {      
-      Stack<Integer> st = new Stack<>();      
+      Stack<Integer> st = new Stack<>();     //Only has index of opening braces 
       char[] temp = s.toCharArray();
       for (int i = 0; i < temp.length; ++i) 
       {
@@ -10,18 +10,18 @@
         }          
         else if (temp[i] == ')') 
         {
-          if (!st.empty()) 
+          if (!st.empty())  //since we only keep opening braces index in stack, if not empty means there's matching opening brace for this closing
           {
               st.pop();
           }
           else 
           {             
-             temp[i] = ' ';
+             temp[i] = ' ';      //if empty, no opening brace to match
           }
         }
       }
         
-      while (!st.empty())
+      while (!st.empty())     //when we have tooo many opening braces, mark their indices as empty
            temp[st.pop()] = ' ';        
         
       StringBuilder bu = new StringBuilder();
@@ -30,4 +30,42 @@
               bu.append(ch);
       
       return bu.toString();     
+    }
+
+
+//Solution 2
+
+ public String minRemoveToMakeValid(String s) 
+    { 
+      //scan forards, remove invalid closing
+      //scan backwards, remove invalid opening
+      // reverse result    
+      int opens = 0;      
+      StringBuilder bu = new StringBuilder();
+      for (char c :  s.toCharArray()) 
+      {
+        if (c == '(') 
+        {
+           opens++;
+        }          
+        else if (c == ')') 
+        {
+          if (opens == 0) 
+          {
+              continue;
+          }         
+          opens--;
+        }
+
+        bu.append(c);
+      }
+      
+      StringBuilder result = new StringBuilder();
+      for(int i = bu.length() - 1; i >= 0; i--){
+            if(bu.charAt(i) == '(' && opens-- > 0)
+               continue;
+            result.append(bu.charAt(i));
+      }
+      
+      return result.reverse().toString();     
     }
