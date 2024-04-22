8f78276f84016cae5c1643b8974da048d98f270d
otubukhay <33136805+otubukhay@users.noreply.github.com>
2024-04-21 19:48:57 -0500

    Update 314. Binary Tree Vertical Order Traversal.java

--- 314. Binary Tree Vertical Order Traversal.java
+++ 314. Binary Tree Vertical Order Traversal.java
@@ -1,37 +1,44 @@
-  public List<List<Integer>> verticalOrder(TreeNode root)
+class Solution {
+     public List<List<Integer>> verticalOrder(TreeNode root)
   {
         List<List<Integer>> res = new ArrayList<>();
         if(root == null) 
 			return res;
-        Map<Integer, List<Integer>> dict = new TreeMap<>();
+        Map<Integer, List<Integer>> map = new HashMap<>(); // Using treemap with cause look up to logn, hence solution nlogn
         Queue<TreeNode> queue = new LinkedList<>();
         Queue<Integer> code = new LinkedList<>();
 
         queue.offer(root);
         code.offer(0);
+
+        int min = 0; //use min/max to keep track of smallest and highest window
+        int max = 0;
         while(!queue.isEmpty())
-		{
+	{
             TreeNode front = queue.poll();
             int val = code.poll();
-            if(!dict.containsKey(val))
-			{
-                dict.put(val, new ArrayList<>());
-            }
-			
-            dict.get(val).add(front.val);
+            map.putIfAbsent(val, new ArrayList<>());			
+            map.get(val).add(front.val);
+            
             if(front.left != null)
-			{
+	    {
                 queue.offer(front.left);
                 code.offer(val - 1);
+                min = Math.min(min, val - 1);
             }
 			
-            if(front.right != null)
-			{
+           if(front.right != null)
+	   {
                 queue.offer(front.right);
                 code.offer(val + 1);
+                max = Math.max(max, val + 1);
             }
         }
-        res.addAll(dict.values());
-        return res;
-
+      
+        for (int i = min; i <= max; i++) {
+           res.add(map.get(i));
+        }
+        return res;      
     }
+}
+
