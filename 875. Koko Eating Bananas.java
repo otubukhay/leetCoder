22c3b8d9c358e79ee8fe96e715e088c85cf538da
otubukhay <33136805+otubukhay@users.noreply.github.com>
2024-12-15 01:58:55 -0600

    Update 875. Koko Eating Bananas.java

--- 875. Koko Eating Bananas.java
+++ 875. Koko Eating Bananas.java
@@ -1,39 +1,30 @@
- public int minEatingSpeed(int[] piles, int H) 
- {        
-        int low = 1;
-        int high = -1;        
-        
-        for(int pile : piles)
-		{
-            high = Math.max(high, pile);
-		}
-        
-        int minimum = high;        
-        while(low <= high)
-        {
-            int mid =  low + (high - low)/2;
-            if(canFinish(piles, mid, H))
-            {
-               minimum = mid;
-               high = mid - 1;
-            }
-            else
-            {
-               low = mid + 1;
-            }
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 0;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
         }
         
-        return minimum;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canEatAll(piles, mid, h)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
     }
-    
-    boolean canFinish(int[] piles, int speed, int hours)
-    {
+
+    boolean canEatAll(int[] piles, int speed, int hours) {
         int timeTaken = 0;
-        for(int pile : piles)
-        {            
-            timeTaken += pile/speed;
-            timeTaken += ((pile % speed > 0) ?  1 : 0);
+        for (int pile : piles) {
+            timeTaken += pile / speed;
+            timeTaken += ((pile % speed > 0) ? 1 : 0);
         }
-        
+
         return timeTaken <= hours;
     }
+}
